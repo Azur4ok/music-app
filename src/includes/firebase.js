b7d440228fff7e94972b3aea04b2d064166c9c98
Aziz Isanov <alberkrush322@gmail.com>
2023-05-06 21:31:57 +0300

    configurated and added firebase auth and firestore

---
+++ src/includes/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDogEJiBJE4AoisEfo38byYUhaYhf4KPyo',
+  authDomain: 'music-app-3783c.firebaseapp.com',
+  projectId: 'music-app-3783c',
+  storageBucket: 'music-app-3783c.appspot.com',
+  messagingSenderId: '969887905268',
+  appId: '1:969887905268:web:c69a8e32e6a293998575a1'
+}
+
+const app = initializeApp(firebaseConfig)
+
+const db = getFirestore(app)
+
+export { app, db }
